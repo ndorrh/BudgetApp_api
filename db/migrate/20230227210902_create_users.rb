b7ee46014742f574005c371d71c269cc5de3ec7f
ndorrh <ndorrh@gmail.com>
2023-02-27 22:21:04 +0100

    Modify migration file and create migration to update database schema

--- db/migrate/20230227210902_create_users.rb
+++ db/migrate/20230227210902_create_users.rb
@@ -2,13 +2,11 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :user_name, null: false, default: ''
-      t.string :email, null: false, default: ''
+      t.string :user_name, null: false, default: '', index: { unique: true }
+      t.string :email, null: false, default: '', index: { unique: true }
       t.string :Password_digest, null: false, default: ''
 
       t.timestamps
     end
   end
-  add_index :users, :email, unique: true
-  add_index :users, :user_name, unique: true
 end
