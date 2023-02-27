a45c2746e243586dbe5ca0883219d27eed9918d3
ndorrh <ndorrh@gmail.com>
2023-02-27 22:25:56 +0100

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  #Add validation to attributes
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :user_name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
