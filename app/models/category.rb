a8a55dca224c7aac18dce61dd4bdc5aa35d90a74
ndorrh <ndorrh@gmail.com>
2023-02-28 17:00:01 +0100

    Add association and validation to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  has_many :category_transactions, dependent: :destroy
+  has_many :my_transactions, through: :category_transactions
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
