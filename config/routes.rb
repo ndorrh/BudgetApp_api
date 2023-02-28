cf6d494fec94f13cf23cb5c5d32791916e1a226b
ndorrh <ndorrh@gmail.com>
2023-02-28 16:27:50 +0100

    Add categories resourses to route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :my_transactions
+      resources :categories
       post '/auth/login', to: 'authentication#login'
     end
   end
