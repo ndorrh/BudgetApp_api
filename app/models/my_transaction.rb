a9d5a20eb8d3a8c555eaa28bd88ebfce23d70ee8
ndorrh <ndorrh@gmail.com>
2023-02-28 17:00:44 +0100

    Add more association and validation to MyTransaction model

--- app/models/my_transaction.rb
+++ app/models/my_transaction.rb
@@ -1,3 +1,8 @@
 class MyTransaction < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :category_transactions, dependent: :destroy
+  has_many :categories, through: :category_transactions
+
+   validates :name, presence: true
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
