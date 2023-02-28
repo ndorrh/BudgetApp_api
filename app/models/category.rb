34a2f8526830da22d86553ece2ac231401553ad0
ndorrh <ndorrh@gmail.com>
2023-02-28 18:10:45 +0100

    Modify association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :category_transactions, dependent: :destroy
-  has_many :my_transactions, through: :category_transactions
+  has_many :my_transactions, through: :category_transactions, dependent: :destroy
 
   validates :name, presence: true
   validates :icon, presence: true
